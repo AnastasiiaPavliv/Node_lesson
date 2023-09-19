4743fe71aaccfa9f625fe458b8258cdd3f06d416
AnastasiiaPavliv <pavlivnastia@gmail.com>
2023-09-19 12:13:52 +0300

    hw1

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const path = require("path");
 
 const filePAth = path.join(__dirname, 'test')
 
-fs.readdir= 
+fs.readdir(filePAth,{withFileTypes:true},(err, files)=>{
+    if (err) {
+        throw new Error(err.message)
+    }else{
+        files.forEach(file=>{
+            if (file.isDirectory()){
+                console.log(  file.name + ' It is folder')
+            }else {
+                console.log( file.name + ' It is file')
+            }
+
+        })
+    }
+})
 
 console.log(filePAth)
