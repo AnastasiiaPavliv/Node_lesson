73fc00bcde331e0b73e0f4eb8f74f7e0a23c100b
AnastasiiaPavliv <pavlivnastia@gmail.com>
2023-09-29 00:40:01 +0300

    lesson3

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-import express from "express";
+import express, {Request} from "express";
+import * as mongoose from "mongoose";
 
+import { configs } from "./configs/config";
 import * as fsService from "./fs.service";
+import {User} from "./models/user.model";
+import {IUser} from "./types/user.type";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/users", async (req, res) => {
-  const users = await fsService.reader();
-  res.json(users);
+  const users = await User.find();
+   res.json(users);
 });
+
 app.post("/users", async (req, res) => {
   const { name, email } = req.body;
   try {
@@ -26,14 +32,17 @@ app.post("/users", async (req, res) => {
     res.status(201).json(newUser);
   } catch {
     res.status(400).json();
+
   }
 });
+
 app.get("/users/:id", async (req, res) => {
   const { id } = req.params;
   const users = await fsService.reader();
   const user = users.find((user) => user.id === Number(id));
   res.json(user);
 });
+
 app.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
   const users = await fsService.reader();
@@ -44,7 +53,8 @@ app.delete("/users/:id", async (req, res) => {
   res.sendStatus(204);
 });
 
-const PORT = 5001;
-app.listen(PORT, () => {
+const PORT = 5000;
+app.listen(PORT, async () => {
+  await mongoose.connect(configs.DB_URI);
   console.log(`Server OK ${PORT}`);
 });
